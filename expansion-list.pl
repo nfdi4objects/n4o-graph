b22c3268be671dae5a75ecfc73fe50cfbc863c64
Jakob Voss <voss@gbv.de>
2024-05-14 13:56:28 +0200

    Expand CRM properties

--- expansion-list.pl
+++ expansion-list.pl
@@ -10,22 +10,28 @@ while (<>) {
 }
 
 sub id {
-    ( split "_", $_[0] )[0];
+    $_[0] =~ /^([PE]\d+)_/ ? $1 : $_[0];
 }
-
-say "E1_CRM_Entity E1";
 
 # get reachable classes for each class
 for my $start ( keys %graph ) {
     my %exp;
-    $exp{ id($start) } = 1;
-    my @queue = $graph{$start};    # may have same ID (rename)
+    my @queue;
+
+    my $short = id($start);
+    $exp{$short} = 1 if $short ne $start;
+
+    # TODO: also include mappings to CRM from other ontologies
+    push @queue, $graph{$start} if $graph{$start} ne $start;
+
     while ( my $name = pop @queue ) {
         my $c = id($name);
         if ( !$exp{$c} || $c == id($start) ) {
             $exp{$c} = 1;
-            push @queue, $graph{$name} if $graph{$name};
+            push @queue, $graph{$name}
+              if $graph{$name} and $graph{$name} ne $name;
         }
     }
-    say join( " ", $start, sort keys %exp );
+
+    say join( " ", $start, sort keys %exp ) if keys %exp;
 }
