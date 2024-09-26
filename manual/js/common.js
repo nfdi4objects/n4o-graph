802751de02441aa4b6873da817e8a497ec5529c0
Jakob Voss <voss@gbv.de>
2024-09-26 12:48:06 +0200

    Show triples count

--- manual/js/common.js
+++ manual/js/common.js
@@ -7,3 +7,12 @@ async function cypherQuery(query) {
     return data
   })
 }
+
+async function sparqlQuery(body) {
+  return fetch('https://graph.nfdi4objects.net/api/sparql', {method: "POST", body, headers: { "Content-Type": "application/sparql-query" }}).then(async response => {
+    const data = await response.json()
+    if (!response.ok) throw new Error(`${data.error}: ${data.message}`)
+    if (!Array.isArray(data)) throw new TypeError("Malformed API response")
+    return data
+  })
+}
