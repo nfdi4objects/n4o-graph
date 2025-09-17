9f998065401c186043868aaf30054f51a1ac77a4
Jakob Voss <voss@gbv.de>
2025-09-17 10:51:23 +0200

    Workshop: extract mappings

--- workshop/init.sh
+++ workshop/init.sh
@@ -6,12 +6,13 @@ apis=http://localhost:8000/
 
 POST() { curl --silent --fail -X POST "$@"; }
 PUT() { curl --silent --fail -X PUT "$@"; }
+download() { wget --quiet -N --no-if-modified-since "$@"; } 
 
 echo "## Register terminologies"
 PUT -d @terminologies.json ${importer}terminology/ | jq -r '.[]|[.uri,.prefLabel.en]|@tsv'
 echo
 echo "## Download and extract AAT hierarchy"
-wget --quiet -N --no-if-modified-since http://aatdownloads.getty.edu/VocabData/explicit.zip
+download http://aatdownloads.getty.edu/VocabData/explicit.zip
 unzip -p explicit.zip AATOut_HierarchicalRels.nt | \
     awk '$2~/broader/ {print $1" <http://www.w3.org/2004/02/skos/core#broader> "$3"."}' > ../data/aat-hierarchy.nt
 wc -l ../data/aat-hierarchy.nt
@@ -25,10 +26,15 @@ POST ${importer}terminology/75/load
 
 echo
 echo "## Receive and load KENOM Material"
-POST ${importer}terminology/20533/receive?from=https://api.dante.gbv.de/export/download/kenom_material/default/kenom_material__default.jskos.ndjson
+url=https://api.dante.gbv.de/export/download/kenom_material/default/kenom_material__default.jskos.ndjson
+POST ${importer}terminology/20533/receive?from=$url
 POST ${importer}terminology/75/load
 
-# TODO: receive and load KENOM Material
+echo
+echo "## Download and extract embedded 1-to-1 mappings from KENOM Material"
+download $url
+jq -r '.mappings|select(.)|.[]|"<\(.from.memberSet[0].uri)> <\(.type[0])> <\(.to.memberSet[0].uri)> ."' \
+    kenom_material__default.jskos.ndjson > ../data/kenom_material-mappings.nt
 
-# TODO: register, receive and load mappings between AAT and GND and/or KENOM Material
+# TODO: register, receive and load mappings
 
